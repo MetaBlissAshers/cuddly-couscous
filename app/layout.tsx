7b756194d152226aacc2f58db9add8b4ffcdf45c
Your Name <your_email@example.com>
2025-08-05 20:24:06 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import { LocationLessAds } from "./components/LocationLessAds";
+// import { LocationLessAds } from "./components/LocationLessAds";
 // import AgeVerification from "./components/AgeVerification";
 
 const geistSans = Geist({
